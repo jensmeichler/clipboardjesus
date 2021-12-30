6f3d57eb7c9ca1e38c8a79b9b066b3d1cf3c0b0e
jensmeichler <jensmeichler@live.com>
2021-12-30 14:57:57 +0100

    fix: removed flicker while dragging

--- src/app/directives/custom-drag-drop.directive.ts
+++ src/app/directives/custom-drag-drop.directive.ts
@@ -4,26 +4,50 @@ import {Directive, HostBinding, HostListener} from '@angular/core';
   selector: '[customDragDrop]'
 })
 export class CustomDragDropDirective {
+  counter = 0;
+
   @HostBinding('class')
   elementClass = 'clipboard';
 
   @HostListener("dragover", ["$event"])
-  @HostListener("dragenter", ["$event"])
-  onDragging(event: any) {
-    this.elementClass = 'clipboard clipboard-hover';
+  onDragOver(event: any) {
     event.preventDefault();
+    this.addClass();
   }
 
-  @HostListener("dragend", ["$event"])
-  @HostListener("dragleave", ["$event"])
-  onNotDragging(event: any) {
-    this.elementClass = 'clipboard';
+  @HostListener("dragenter", ["$event"])
+  onDragEnter(event: any) {
     event.preventDefault();
+    this.counter++;
+    this.addClass();
+  }
+
+  @HostListener("dragend")
+  onDragEnd() {
+    this.removeClass();
+  }
+
+  @HostListener("dragleave")
+  onDragLeave() {
+    this.counter--;
+    if (this.counter === 0) {
+      this.removeClass();
+    }
   }
 
   @HostListener("drop", ["$event"])
   onDrop(event: any) {
-    this.onNotDragging(event)
+    event.preventDefault();
     event.stopPropagation();
+    this.removeClass();
+  }
+
+  private addClass() {
+    this.elementClass = 'clipboard clipboard-hover';
+  }
+
+  private removeClass() {
+    this.elementClass = 'clipboard';
+    this.counter = 0;
   }
 }
