60cc1f60b160115d5558d98d2570155f0e5dedc4
jensmeichler <jensmeichler@live.com>
2022-01-05 20:37:54 +0100

    fix: edited regex for links

--- src/app/const/regexes.ts
+++ src/app/const/regexes.ts
@@ -1,2 +1,2 @@
 export const colorRegex = new RegExp(/^#([a-fA-F0-9]{8}|[a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/);
-export const hyperlinkRegex = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+export const hyperlinkRegex = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\-\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\-\+.~#?&//=]*)/);
