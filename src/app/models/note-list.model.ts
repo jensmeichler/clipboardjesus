75e2e3d50d778e6f9692a49d6d66e909b25c8f79
jensmeichler <jensmeichler@live.com>
2022-05-10 23:00:29 +0200

    feat: edit note list dialog with foreground and background

--- src/app/models/note-list.model.ts
+++ src/app/models/note-list.model.ts
@@ -4,6 +4,9 @@ import {DraggableNote} from "./draggable-note.model";
 export class NoteList implements DraggableNote {
   notes: Note[] = [];
   header?: string;
+  foregroundColor: string = '#ffffff';
+  backgroundColor: string = '#212121';
+  backgroundColorGradient?: string;
   posX: number;
   posY: number;
   posZ?: number;
