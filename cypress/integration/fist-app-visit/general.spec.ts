e6d38ee99df0914118fa36effbaea3e364328d3c
Jens Meichler <50821093+jensmeichler@users.noreply.github.com>
2022-05-21 23:51:09 +0200

    test: cypress note tests (#15)
    * ci: github pages deploy just on push to develop
    
    * test: cypress setup
    
    * test: notes test definitions
    
    * test: notes test definitions
    
    * test: note test implementation
    
    * test: note test implementation
    
    * test: custom cmd for dataCy
    
    * test: added test ids
    
    * test: check required buttons

--- cypress/integration/fist-app-visit/general.spec.ts
+++ cypress/integration/fist-app-visit/general.spec.ts
@@ -1,46 +1,58 @@
 /// <reference types="cypress" />
 
-context('First time visiting app', () => {
+context('First time visiting visit-with-data', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/');
   })
 
   describe('Layout checks', () => {
     it('should contain required buttons', () => {
-      cy.get('#logo').should('exist');
-      cy.get('#save-button').should('exist');
-      cy.get('#save-as-button').should('exist');
-      cy.get('#settings-button').should('exist');
+      cy.dataCy('logo').should('exist');
+      cy.dataCy('save-button').should('exist');
+      cy.dataCy('save-as-button').should('exist');
+      cy.dataCy('settings-button').should('exist');
     })
 
     it('should contain a single tab', () => {
-      cy.get('cb-tab').should('exist').and('have.length', 1);
+      cy.dataCy('tab').should('exist').and('have.length', 1);
     })
 
     it('should not be able to save empty board', () => {
-      cy.get('#save-button').should('be.disabled');
-      cy.get('#save-as-button').should('be.disabled');
+      cy.dataCy('save-button').should('be.disabled');
+      cy.dataCy('save-as-button').should('be.disabled');
     })
 
     it('should not contain items', () => {
-      cy.get('cb-note').should('not.exist');
-      cy.get('cb-task-list').should('not.exist');
-      cy.get('cb-note-list').should('not.exist');
-      cy.get('cb-image').should('not.exist');
+      cy.dataCy('note').should('not.exist');
+      cy.dataCy('task-list').should('not.exist');
+      cy.dataCy('note-list').should('not.exist');
+      cy.dataCy('image').should('not.exist');
     })
   })
 
   describe('Functionality checks', () => {
     it('should open context menu on right click', () => {
-      cy.get('#tab-context-menu').should('not.exist');
-      cy.get('cb-tab').rightclick();
-      cy.get('#tab-context-menu').should('be.visible');
+      cy.dataCy('tab-context-menu').should('not.exist');
+      cy.dataCy('tab').rightclick();
+      cy.dataCy('tab-context-menu').should('be.visible');
     })
 
     it('should open about dialog on logo click', () => {
-      cy.get('cb-about-dialog').should('not.exist');
-      cy.get('#logo').click();
-      cy.get('cb-about-dialog').should('be.visible');
+      cy.dataCy('about-dialog').should('not.exist');
+      cy.dataCy('logo').click();
+      cy.dataCy('about-dialog').should('be.visible');
+    })
+
+    xit('should be localized in english', () => {
+      //TODO
+    })
+
+    xit('should be localized in german', () => {
+      //TODO
+    })
+
+    xit('should be localized in ukrainian', () => {
+      //TODO
     })
   })
 })
