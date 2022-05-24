47170cf14a7284c240a6eebf943b48c269ef73eb
Jens Meichler <jensmeichler@live.com>
2022-05-24 06:51:02 +0200

    test: import notes and tabs

---
+++ cypress/integration/import-functionality.spec.ts
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+import manyNotesTab from '../fixtures/many-notes.json';
+import {dataCy} from "../support/selectors";
+
+context('Localization functionality', () => {
+  let multicoloredNotesCount: number;
+  let yellowNotesCount: number;
+  let purpleNotesCount: number;
+
+  beforeEach(() => {
+    const yellowNotesTab = {...manyNotesTab, notes: manyNotesTab.notes.filter(x => x.content === 'yellow')};
+    const purpleNotesTab = {...manyNotesTab, notes: manyNotesTab.notes.filter(x => x.content === 'blue')};
+    multicoloredNotesCount = manyNotesTab.notes.length;
+    yellowNotesCount = yellowNotesTab.notes.length;
+    purpleNotesCount = purpleNotesTab.notes.length;
+
+    cy.seed([manyNotesTab, yellowNotesTab, purpleNotesTab]);
+    cy.visit('/');
+  })
+
+  it('should import all the notes from localstorage', () => {
+    cy.dataCy(dataCy.note.note).should('have.length', multicoloredNotesCount);
+
+    // Go to yellow notes tab
+    cy.dataCy(dataCy.tab.other).first().click();
+    cy.dataCy(dataCy.note.note).should('have.length', yellowNotesCount);
+
+    // Go to blue notes tab
+    cy.dataCy(dataCy.tab.other).last().click();
+    cy.dataCy(dataCy.note.note).should('have.length', purpleNotesCount);
+
+    // Go to multicolored notes tab
+    cy.dataCy(dataCy.tab.other).first().click();
+    cy.dataCy(dataCy.note.note).should('have.length', multicoloredNotesCount);
+  })
+})
--- cypress/integration/localization-functionality.spec.ts
+++ cypress/integration/localization-functionality.spec.ts
@@ -4,16 +4,18 @@ import {dataCy, selectors} from "../support/selectors";
 
 context('Localization functionality', () => {
   it('should be localized in english, german, ukrainian', () => {
-    cy.lang('en');
+    // Default language should be english
     cy.visit('/');
     cy.dataCy(dataCy.common.buttons.settings).click();
     cy.get(selectors.menu).should('contain', 'Switch language');
 
+    // Localization in german should work
     cy.lang('de');
     cy.visit('/');
     cy.dataCy(dataCy.common.buttons.settings).click();
     cy.get(selectors.menu).should('contain', 'Sprache wechseln');
 
+    // Localization in ukrainian should work
     cy.lang('ua');
     cy.visit('/');
     cy.dataCy(dataCy.common.buttons.settings).click();
