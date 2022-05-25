ba72ab113f5adfb7de7771d89c6652f617d2a7f2
Jens Meichler <j.meichler@sternico.com>
2022-05-25 19:15:16 +0200

    refactor: removed moment plugin ➖

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import * as moment from "moment";
 import {FileSaverService} from "ngx-filesaver";
 
 @Injectable({
@@ -10,11 +9,24 @@ export class FileService {
   }
 
   save(content: string, fileType: string, filename?: string): string {
-    filename ??= moment(new Date()).format('YYYY-MM-DD-HH-mm');
+    filename ??= FileService.getTimeStamp();
     filename = filename.endsWith('.' + fileType) ? filename : filename + '.' + fileType
-
     this.fileSaverService.saveText(content, filename);
-
     return filename;
+  }
+
+  private static getTimeStamp(): string {
+    const date = new Date();
+    const yyyy = date.getFullYear();
+    const MM = FileService.trim(date.getMonth() + 1);
+    const dd = FileService.trim(date.getDate());
+    const HH = FileService.trim(date.getHours());
+    const mm = FileService.trim(date.getMinutes());
+    const ss = FileService.trim(date.getSeconds());
+    return `${yyyy}-${MM}-${dd}_${HH}-${mm}-${ss}`;
+  }
+
+  private static trim(value: number): string {
+    return ('0' + value).slice(-2);
   }
 }
