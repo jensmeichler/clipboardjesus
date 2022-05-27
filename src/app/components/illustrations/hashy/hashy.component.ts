d87730aa213107b599ce2ab04e48a115b22240ea
Jens Meichler <j.meichler@sternico.com>
2022-05-27 20:05:10 +0200

    style: removed empty functions 👕

--- src/app/components/illustrations/hashy/hashy.component.ts
+++ src/app/components/illustrations/hashy/hashy.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'cb-hashy',
   templateUrl: './hashy.component.html',
   styleUrls: ['./hashy.component.scss']
 })
-export class HashyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HashyComponent {
 }
