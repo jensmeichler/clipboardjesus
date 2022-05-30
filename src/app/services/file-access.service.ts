cefb561fe1d4d87a67b7990be68e4df613d44022
Jens Meichler <j.meichler@sternico.com>
2022-05-30 07:52:04 +0200

    feat: saved current filePath 🚧

--- src/app/services/file-access.service.ts
+++ src/app/services/file-access.service.ts
@@ -3,12 +3,16 @@ import {FsTextFileOption} from "@tauri-apps/api/fs";
 
 @Injectable({providedIn: 'root'})
 export class FileAccessService {
+  loadedPath = '';
+
   async read(path: string): Promise<string> {
+    this.loadedPath = path;
     // @ts-ignore https://tauri.studio/v1/api/js/modules/fs/
     return window.__TAURI__.fs.readTextFile(path);
   }
 
-  write(contents: string, path: string): void {
+  write(contents: string, path?: string): void {
+    path ??= this.loadedPath;
     const file: FsTextFileOption = {path, contents};
     // @ts-ignore https://tauri.studio/v1/api/js/modules/fs/
     window.__TAURI__.fs.writeFile(file);
