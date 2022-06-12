313c46f1cb368cb7742cbe70df0fd711dcff232d
Jens Meichler <50821093+jensmeichler@users.noreply.github.com>
2022-06-12 16:56:09 +0200

    chore(deps): angular update to v14 ⚡ (#49)
    * feat: updating angular ⚡
    
    * feat: updating angular ⚡
    
    * feat: updating angular ⚡
    
    * refactor: edited som structure of e2e 🎨
    
    * fix: suppressed peer deps warning ⚡
    
    * chore: typescript version update ⚡
    
    * chore: tauri app version 🔖
    
    * ci: dont fail fast on web build pipeline 👷
    
    * ci: clean install for npm install 👷
    
    * chore: regenerated package-lock.json 📦

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'cypress'
+import PluginEvents = Cypress.PluginEvents;
+import PluginConfigOptions = Cypress.PluginConfigOptions;
 
 export default defineConfig({
   projectId: 'c1e5r5',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    setupNodeEvents(on: PluginEvents, config: PluginConfigOptions) {
+      // `on` is used to hook into various events Cypress emits
+      // `config` is the resolved Cypress config
     },
     baseUrl: 'http://localhost:4201',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
   },
 })
--- cypress/plugins/index.js
+++
@@ -1,5 +0,0 @@
-// noinspection JSUnusedLocalSymbols
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
