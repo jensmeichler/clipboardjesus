5e6ecd80dfbe08f6ae7cdc47f374cca42ed6da41
Jens Meichler <j.meichler@sternico.com>
2022-12-27 23:30:58 +0100

    chore: migrated some classes in order to let the app work

--- src/app/const/scrolled-position.ts
+++ src/app/const/scrolled-position.ts
@@ -1,5 +1,5 @@
 export const scrolledPosition = (): {top: number, left: number} => {
-  const tabBody = document.documentElement.getElementsByClassName('mat-tab-body-active')[0];
+  const tabBody = document.documentElement.getElementsByClassName('mat-mdc-tab-body-active')[0];
   const top = tabBody.scrollTop ?? 0;
   const left = tabBody.scrollLeft ?? 0;
   return {top, left};
